4360a32db58e7ebc4b08e3bd1684206dc0014982
Lyndi Holzwarth <lyndi.holzwarth@gmail.com>
2015-05-27 21:05:12 -0400

    params written, no code working

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ $(document).ready( function() {
 		var tags = $(this).find("input[name='tags']").val();
 		getUnanswered(tags);
 	});
+	$('.inspiration-getter').submit(function(event){
+		$('.results').html('');
+		var answerers = $(this).find("input[name='answerers']").val();
+		// console.log(answerers)
+		getInspired(answerers);
+	})
 });
 
 // this function takes the question object returned by StackOverflow 
@@ -14,6 +20,39 @@ var showQuestion = function(question) {
 	
 	// clone our result template code
 	var result = $('.templates .question').clone();
+	
+	// Set the question properties in result
+	var questionElem = result.find('.question-text a');
+	questionElem.attr('href', question.link);
+	questionElem.text(question.title);
+
+	// set the date asked property in result
+	var asked = result.find('.asked-date');
+	var date = new Date(1000*question.creation_date);
+	asked.text(date.toString());
+
+	// set the #views for question property in result
+	var viewed = result.find('.viewed');
+	viewed.text(question.view_count);
+
+	// set some properties related to asker
+	var asker = result.find('.asker');
+	asker.html('<p>Name: <a target="_blank" href=http://stackoverflow.com/users/' + question.owner.user_id + ' >' +
+													question.owner.display_name +
+												'</a>' +
+							'</p>' +
+ 							'<p>Reputation: ' + question.owner.reputation + '</p>'
+	);
+
+	return result;
+};
+
+// this function takes the answerer object returned by StackOverflow 
+// and creates new result to be appended to DOM
+var showAnswerer = function(question) {
+	
+	// clone our result template code
+	var inspiredResult = $('.templates .answerer').clone();
 	
 	// Set the question properties in result
 	var questionElem = result.find('.question-text a');
@@ -62,9 +101,9 @@ var getUnanswered = function(tags) {
 	
 	// the parameters we need to pass in our request to StackOverflow's API
 	var request = {tagged: tags,
-								site: 'stackoverflow',
-								order: 'desc',
-								sort: 'creation'};
+					site: 'stackoverflow',
+					order: 'desc',
+					sort: 'creation'};
 	
 	var result = $.ajax({
 		url: "http://api.stackexchange.com/2.2/questions/unanswered",
@@ -74,19 +113,53 @@ var getUnanswered = function(tags) {
 		})
 	.done(function(result){
 		var searchResults = showSearchResults(request.tagged, result.items.length);
+		console.log(searchResults)
 
-		$('.search-results').html(searchResults);
+	// 	$('.search-results').html(searchResults);
 
-		$.each(result.items, function(i, item) {
-			var question = showQuestion(item);
-			$('.results').append(question);
-		});
-	})
-	.fail(function(jqXHR, error, errorThrown){
-		var errorElem = showError(error);
-		$('.search-results').append(errorElem);
+	// 	$.each(result.items, function(i, item) {
+	// 		console.log(item);
+	// 		var question = showQuestion(item);
+	// 		$('.results').append(question);
+	// 	});
+	// })
+	// .fail(function(jqXHR, error, errorThrown){
+	// 	var errorElem = showError(error);
+	// 	$('.search-results').append(errorElem);
 	});
+	// console.log("unanswered"+result);
+
 };
 
+	function getInspired(answerers){
+	// // the parameters we need to pass in our request to StackOverflow's API
+	var inspiredRequest = {
+		tag: answerers,
+		site: 'stackoverflow',
+		period: 'month',
+	};
+	
+	var inspiredResult = $.ajax({
+		url:'http://api.stackexchange.com/2.2/tags/'+answerers+'/top-answerers',
+		data: inspiredRequest,
+		dataType: "jsonp",
+		type: "GET",
+		})
+	.done(function(result){
+		var inspiredSearchResults = showSearchResults(inspiredRequest.tag, inspiredResult.items.length);
+		console.log(searchResults);
+	// 	$('.search-results').html(searchResults);
+
+	// 	$.each(inspiredResult.items, function(i, item) {
+	// 		console.log(item)
+	// 		// var question = showAnswerer(item);
+	// 		// $('.results').append(question);
+	// 	});
+	})
+	// .fail(function(jqXHR, error, errorThrown){
+	// 	var errorElem = showError(error);
+	// 	$('.search-results').append(errorElem);
+	// });
+};
 
 
