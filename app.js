7192de6f4fa94774f5f3a2360f6e63c0929a9948
Lyndi Holzwarth <lyndi.holzwarth@gmail.com>
2015-05-31 14:50:03 -0400

    basic name listing

--- app.js
+++ app.js
@@ -47,6 +47,37 @@ var showQuestion = function(question) {
 	return result;
 };
 
+var showAnswerer= function(question) {
+	// html+='<p>'+testTest+'</p>';
+	// // clone our result template code
+	var result = $('.templates .answerer').clone();
+	
+	// // Set the question properties in result
+	// var questionElem = result.find('.question-text a');
+	// questionElem.attr('href', question.link);
+	// questionElem.text(question.title);
+
+	// // set the date asked property in result
+	// var asked = result.find('.asked-date');
+	// var date = new Date(1000*question.creation_date);
+	// asked.text(date.toString());
+
+	// // set the #views for question property in result
+	// var viewed = result.find('.viewed');
+	// viewed.text(question.view_count);
+
+	// // set some properties related to asker
+	// var asker = result.find('.asker');
+	// asker.html('<p>Name: <a target="_blank" href=http://stackoverflow.com/users/' + question.owner.user_id + ' >' +
+	// 												question.owner.display_name +
+	// 											'</a>' +
+	// 						'</p>' +
+ // 							'<p>Reputation: ' + question.owner.reputation + '</p>'
+	// );
+
+	// return result;
+};
+
 // this function takes the results object from StackOverflow
 // and creates info about search results to be appended to DOM
 var showSearchResults = function(query, resultNum) {
@@ -100,49 +131,33 @@ var getUnanswered = function(tags) {
 	function getInspired(answerers){
 	// // the parameters we need to pass in our request to StackOverflow's API
 	var inspiredRequest = {
-		// tag: answerers,
-		// period: 'month',
 		site: 'stackoverflow',
 	};
-	url='http://api.stackexchange.com/2.2/tags/'+answerers+'/top-answerers/month';
-	$.getJSON(url, inspiredRequest, function(data){
-		showResults(data.items);
-
-	// 		var inspiredResult = $.ajax({
-	// 		// url:'http://api.stackexchange.com/2.2/tags/'+answerers+'/top-answerers',
-	// 		data: inspiredRequest,
-	// 		dataType: "jsonp",
-	// 		type: "GET",
-	// 		})
-	// });
-	});
-};
-	// myData = data.items;
-	function showResults(results){
-		var html="";
-	$.each(results, function(index,value){
-		html+='<p>'+value.user.display_name+'</p>';
-		console.log(value.user.display_name);
-	});
-	$('.html').html(html);
-};	
-// data.items[0].user.display_name
-
-	// .done(function(result){
+	var inspiredResult = $.ajax({
+	url:'http://api.stackexchange.com/2.2/tags/'+answerers+'/top-answerers/month',
+	data: inspiredRequest,
+	dataType: "jsonp",
+	type: "GET",
+	})
+	.done(function(inspiredResult){
+		// showResults(data.items);
 	// 	var inspiredSearchResults = showSearchResults(inspiredRequest.tag, inspiredResult.items.length);
 	// 	console.log(searchResults);
 	// 	$('.search-results').html(searchResults);
-
-	// 	$.each(inspiredResult.items, function(i, item) {
-	// 		console.log(item)
-	// 		// var question = showAnswerer(item);
-	// 		// $('.results').append(question);
-	// 	});
-	// })
-	// .fail(function(jqXHR, error, errorThrown){
-	// 	var errorElem = showError(error);
-	// 	$('.search-results').append(errorElem);
-	// });
-// };
+		html="";
+		$.each(inspiredResult.items, function(index,value){
+			AnswererName=value.user.display_name
+			// var answerersTag = showAnswerer(AnswererName);
+			// $('.results').append(answerersTag);
+			// html+='<p>'+value.user.display_name+'</p>';
+			console.log(AnswererName);
+		});
+		$('.html').html(html);	
+	})
+	.fail(function(jqXHR, error, errorThrown){
+		var errorElem = showError(error);
+		$('.search-results').append(errorElem);
+	});
+};
 
 // search by tags, find top-answerer sorted by rep(?), name, answered question, date
